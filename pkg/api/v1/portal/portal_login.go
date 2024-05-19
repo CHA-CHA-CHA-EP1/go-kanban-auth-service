44d0760a0420f7ecdd134ac3f4eb2845986c9f72
Suthiphong Thaisuriya <a677152@AR8030677152N1.local>
2024-05-20 00:46:11 +0700

    Update common-service, implement login vaidator

--- pkg/api/v1/portal/portal_login.go
+++ pkg/api/v1/portal/portal_login.go
@@ -1,30 +1,27 @@
 package portal
 
 import (
-	"fmt"
-
 	"github.com/CHA-CHA-CHA-EP1/go-kanban-common-service/utils/handler"
 	"github.com/labstack/echo/v4"
 )
 
 type loginPortalRequest struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
+	Username string `json:"username" validate:"required"`
+	Password string `json:"password" validate:"required,min=8,max=32"`
 }
 
 func (p *portalHandlerImpl) Login(c echo.Context) error {
-	fmt.Println("user login")
 	var req loginPortalRequest
 
 	body, err := handler.GetBody[loginPortalRequest](c, req)
-
-	fmt.Println(err)
-
 	if err != nil {
 		return c.String(400, err.Error())
 	}
 
-	fmt.Println(body)
+	err = handler.Validate(body)
+	if err != nil {
+		return c.String(400, err.Error())
+	}
 
-	return c.String(200, body.Username)
+	return c.JSON(200, "OK")
 }
